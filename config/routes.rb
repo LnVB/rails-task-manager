a372b4bd0a3be0fd9c0e90fcb85f103b63b23873
Helene Buffet <hebuffet@gmail.com>
2022-11-08 13:19:46 -0500

    view the details of a task

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   # Read all = list all the tasks
   get '/tasks', to: 'tasks#index'
+  # read one = display one task
+  get '/tasks/:id', to: 'tasks#show', as: :task
 
   # Defines the root path route ("/")
   # root "articles#index"
