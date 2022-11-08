01f8a08edfd8d62b89cda55b3306aaf4ff0d5021
Helene Buffet <hebuffet@gmail.com>
2022-11-08 14:33:34 -0500

    edit a task

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,21 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
   # Read all = list all the tasks
   get '/tasks', to: 'tasks#index'
+
+  # create
+  # 1. Provide the form
+  get '/tasks/new', to: 'tasks#new', as: :new_task
+  # 2. Send the form
+  post '/tasks', to: 'tasks#create'
+
+  # update
+  # 1. Provide the form
+  get '/tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # 2. Send the form
+  patch '/tasks/:id', to: 'tasks#update'
+
   # read one = display one task
   get '/tasks/:id', to: 'tasks#show', as: :task
 
